18c9a11f57ecb86fe8434234f9e5e5fc33935e64
Holt Skinner <HoltSkinner@google.com>
2019-01-14 11:45:04 -0600

    Made days of week an iota (enum)

--- main.go
+++ main.go
@@ -4,24 +4,49 @@ import (
 	"fmt"
 )
 
-var weekdays = [7]string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+// Weekday Day of the Week (int)
+type Weekday int
+
+// Days of the week
+const (
+	Sunday Weekday = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+)
+
+func (w Weekday) String() string {
+	return [...]string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}[w]
+}
 
 func main() {
 
 	var month, day, year int
 
-	fmt.Printf("Enter the Month: ")
-	fmt.Scanln(&month)
+	for month < 1 || month > 12 {
+		fmt.Printf("Enter the Month: ")
+		fmt.Scanln(&month)
+	}
 
-	fmt.Printf("Enter the Day: ")
-	fmt.Scanln(&day)
+	// TODO: More error checking
+	for day < 1 || day > 31 {
+		fmt.Printf("Enter the Day: ")
+		fmt.Scanln(&day)
+	}
 
-	fmt.Printf("Enter the year: ")
-	fmt.Scanln(&year)
+	// Gregorian Calendar released in 1582, 3000 arbitrary
+	for year < 1582 || year > 3000 {
+		fmt.Printf("Enter the year: ")
+		fmt.Scanln(&year)
+	}
 
 	fmt.Println(month, day, year)
+
 }
 
-func doomsday() {
-
+func doomsday(month, day, year int) Weekday {
+	return 1
 }
